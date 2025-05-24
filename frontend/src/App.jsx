31c753b1fa820dca3fa6c5e25893f5722d4e4076
Brajesh <281092322419e@gmail.com>
2025-05-24 12:46:41 +0530

    implement login page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import UserLogin from "./pages/UserLogin";
 
+import CaptainLogin from "./pages/CaptainLogin";
+
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
+        <Route path="captain-login" element={<CaptainLogin />} />
         {/*<Route path="/riding" element={<Riding />} />
         <Route path="/captain-riding" element={<CaptainRiding />} /> */}
       </Routes>
---
+++ frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CaptainLogin = () => {
+  return <div>captain login</div>;
+};
+
+export default CaptainLogin;
