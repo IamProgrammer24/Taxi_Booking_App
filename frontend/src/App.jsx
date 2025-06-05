209887af41ed84a6658c836c4bf76726b9008865
Brajesh <281092322419e@gmail.com>
2025-06-05 11:32:25 +0530

    complete user and captain page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Start from "./pages/Start";
 import UserLogin from "./pages/UserLogin";
 
 import CaptainLogin from "./pages/CaptainLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptainSignup from "./pages/CaptainSignu";
 
 const App = () => {
   return (
@@ -11,7 +13,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
         <Route path="captain-login" element={<CaptainLogin />} />
+        <Route path="captain-signup" element={<CaptainSignup />} />
         start form 2.27
         {/*<Route path="/riding" element={<Riding />} />
         <Route path="/captain-riding" element={<CaptainRiding />} /> */}
