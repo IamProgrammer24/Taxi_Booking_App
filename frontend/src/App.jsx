3b9ec5b2a5b0c05b1e2df50a8c418b82a1aa41a9
Brajesh <281092322419e@gmail.com>
2025-06-07 16:41:16 +0530

    Home page UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,14 @@ import UserLogin from "./pages/UserLogin";
 
 import CaptainLogin from "./pages/CaptainLogin";
 import UserSignup from "./pages/UserSignup";
-import CaptainSignup from "./pages/CaptainSignu";
+import CaptainSignup from "./pages/CaptainSignup";
 import Home from "./pages/Home";
 import UserProtectWrapper from "./pages/UserProtectWrapper";
 import UserLogout from "./pages/UserLogout";
+import CaptainHome from "./pages/CaptainHome";
+import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
+import CaptainLogout from "./pages/CaptainLogout";
+import Riding from "./pages/Riding";
 
 const App = () => {
   return (
@@ -17,9 +21,32 @@ const App = () => {
         <Route path="/" element={<Start />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
-        <Route path="/user/logout" element={<UserLogout />} />
+        <Route path="/ride" element={<Riding />} />
+        <Route
+          path="/user/logout"
+          element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }
+        />
         <Route path="captain-login" element={<CaptainLogin />} />
-        <Route path="captain-signup" element={<CaptainSignup />} />
+        <Route
+          path="captain-signup"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainSignup />
+            </CaptainProtectWrapper>
+          }
+        />
+        <Route
+          path="/captain/logout"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainLogout />
+            </CaptainProtectWrapper>
+          }
+        />
         <Route
           path="/home"
           element={
@@ -28,7 +55,15 @@ const App = () => {
             </UserProtectWrapper>
           }
         />
-        start form 2.27
+        <Route
+          path="captain-home"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+          }
+        />
+        start form 3.47
         {/*<Route path="/riding" element={<Riding />} />
         <Route path="/captain-riding" element={<CaptainRiding />} /> */}
       </Routes>
--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-// import { CaptainDataContext } from '../context/CapatainContext'
+import { CaptainDataContext } from "../context/CaptainContext";
 import { useNavigate } from "react-router-dom";
-// import axios from 'axios'
+import axios from "axios";
 
 const CaptainSignup = () => {
   const navigate = useNavigate();
@@ -17,7 +17,7 @@ const CaptainSignup = () => {
   const [vehicleCapacity, setVehicleCapacity] = useState("");
   const [vehicleType, setVehicleType] = useState("");
 
-  //   const { captain, setCaptain } = React.useContext(CaptainDataContext)
+  const { captain, setCaptain } = React.useContext(CaptainDataContext);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -36,14 +36,17 @@ const CaptainSignup = () => {
       },
     };
 
-    // const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/register`, captainData)
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/register`,
+      captainData
+    );
 
-    // if (response.status === 201) {
-    //   const data = response.data
-    //   setCaptain(data.captain)
-    //   localStorage.setItem('token', data.token)
-    //   navigate('/captain-home')
-    // }
+    if (response.status === 201) {
+      const data = response.data;
+      setCaptain(data.captain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
+    }
 
     setEmail("");
     setFirstName("");
